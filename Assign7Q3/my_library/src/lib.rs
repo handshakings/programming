6b2ba669985e90cf28451d752f85d44c271f586a
nawaz4411 <60043564+nawaz4411@users.noreply.github.com>
2020-01-18 19:05:15 +0000

    Add files via upload

---
+++ Assign7Q3/my_library/src/lib.rs
@@ -0,0 +1,8 @@
+
+pub mod university{
+    pub mod department{
+        pub fn show_depart_name(depart_name:&String){
+            println!("Your department is {}",depart_name);
+        }
+    }
+}
